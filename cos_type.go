88bfee13ed19099abf20291b354a25cdf73dc61f
SmartWalle <smartwalle@gmail.com>
2025-08-02 14:09:35 +0800

    feat: 文件调整

--- cos_type.go
+++ cos_type.go
@@ -1,7 +1,17 @@
 package txcos
 
+// PresignedInfo 上传文件预签名信息
 type PresignedInfo struct {
 	UploadURL string            // 上传地址
 	FilePath  string            // 文件路径
 	Header    map[string]string // 上传请求头
+}
+
+// SceneType 场景类型，用于从业务上对文件的使用场景进行分类
+type SceneType int
+
+type Scene struct {
+	SceneType SceneType // 场景类型
+	Path      string    // 存储路径
+	FileExts  []string  // 支持文件类型(文件后缀，如 txt, png)
 }
--- scene.go
+++
@@ -1,9 +0,0 @@
-package txcos
-
-type SceneType int
-
-type Scene struct {
-	SceneType SceneType // 场景类型
-	Path      string    // 存储路径
-	FileExts  []string  // 支持文件类型(文件后缀，如 txt, png)
-}
